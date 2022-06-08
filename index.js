01f1e01f27434f7ee5046ff6afb67812a7f552b9
ndalilavin <vinson.steve@student.moringaschool.com>
2022-06-09 00:52:39 +0300

    commit tree 0caf36559347f71f7e417930ffcb4960ee4bf1e3

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+const scuberHQ = 42;
+
+function distanceFromHqInBlocks(pickupLocation) {
+    if (pickupLocation > 42) {
+        return pickupLocation - scuberHQ;
+    } else {
+        return scuberHQ - pickupLocation;    
+    }
+}
+
+function distanceFromHqInFeet (pickupLocation) {
+    return (parseInt(distanceFromHqInBlocks(pickupLocation), 10) * 264);
+}
+    
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((destination - start) * 264)
+}
+
+function calculatesFarePrice(start, destination){
+
+    const distance = destination - start;
+    
+    const feet = Math.abs(distance) * 264;
+    if (Math.abs(distance) === 1){
+        return 0;
+    }
+    else if (feet <= 2000){
+        return (Math.abs(feet) - 400) * 0.02;
+    }
+    else if (feet < 2500){
+        return 25;
+    }
+    else if (feet > 2500){
+        return 'cannot travel that far'
+    }
+
+}
